5099c5d26ab02d5a5f70a35da7a655dd3be30eb4
landartzhurlok-code <landart.zhur.lok@gmail.com>
2025-08-31 22:34:05 +0300

    Delete assets directory

--- assets/js/animate.js
+++
@@ -1,7 +0,0 @@
-
-document.addEventListener('DOMContentLoaded',()=>{
-  const obs=new IntersectionObserver((entries)=>{
-    entries.forEach(e=>{if(e.isIntersecting)e.target.classList.add('visible')})
-  },{threshold:.1});
-  document.querySelectorAll('.animate').forEach(el=>obs.observe(el));
-});
